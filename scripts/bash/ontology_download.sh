5fe12943126a7c4734dc9ce6ee447b58088d58f1
callahantiff <callahantiff@gmail.com>
2019-03-30 13:35:02 -0600

    adding bash script to download ontologies

---
+++ scripts/bash/ontology_download.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# run script from project root
+path+="resources/ontology_source_list.txt"
+echo ${path}
+
+if ! [[ -e README.md ]]; then
+    echo "Please run from the root of the project."
+    exit 1
+fi
+
+# run python script to download ontologies and generate data source metadata documentation
+python scripts/python/ontology_downloader.py -f "$path"
