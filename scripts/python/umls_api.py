5d2d4ea8dbd5969775424c327d3aefe384ff5cf5
callahantiff <callahantiff@gmail.com>
2020-04-21 19:35:07 -0600

    cleaned up and added typing

--- scripts/python/umls_api.py
+++ scripts/python/umls_api.py
@@ -12,27 +12,24 @@ from time import sleep
 from scripts.python.authentication import *
 
 
-def term_search(term):
+def term_search(term: str):
     """This function queries the UMLS API and returns a list of dictionaries containing the results of the query.
     Additional input/output arguments for query can be found:
     https://documentation.uts.nlm.nih.gov/rest/search/index.html.
 
     Args:
-        term: a string containing a term
+        term: a string containing a term.
 
     Returns:
-        list of dictionaries containing the results of the query
+        A list of dictionaries containing the results of the query.
     """
 
     auth_client = Authentication()
     content_endpoint = '/rest/search/' + 'current'
-    page_number = 0
-    umls_results = []
-    result = ''
+    page_number, umls_results, result = 0, [], ''
 
     while result != 'NO RESULTS':
-        tries = 0
-        r = ''
+        tries, r = 0, ''
 
         while r == '' and tries != 20:
             try:
@@ -51,106 +48,70 @@ def term_search(term):
                 print(e)
                 sys.exit(1)
 
-        # load results
         items = json.loads(r.text)
         json_data = items['result']
 
-        # test if results were found
         if json_data['results'][0]['name'] == 'NO RESULTS':
             result = json_data['results'][0]['name']
         else:
             umls_results.append(json_data['results'][0])
 
     out_message = 'UMLS API returned: %d' % len(umls_results) + ' result(s) for ' + str(term)
-
-    # print('\n')
-    # print('=' * len(out_message))
-    # print(out_message)
-    # print('=' * len(out_message))
-    # print('\n')
+    print('\n')
+    print('=' * len(out_message))
+    print(out_message)
+    print('=' * len(out_message))
+    print('\n')
 
     return umls_results
 
 
-def code_search(code):
-    """
-    This function takes a string containing the UMLS REST API URI, a string containing a source concept code, and a
+def code_search(code: str):
+    """This function takes a string containing the UMLS REST API URI, a string containing a source concept code, and a
     string specifying UMLS version to query (default is set to query the most up to date version). Using this
     information the REST API is queried. Function returns a list of dictionaries containing the results of the query.
     Additional input/output arguments for query can be found:
     https://documentation.uts.nlm.nih.gov/rest/search/index.html.
 
     Args:
-        code: a string containing a source concept code
+        code: A string containing a source concept code.
 
     Returns:
-        list of dictionaries containing the results of the  query
+        A list of dictionaries containing the results of the query.
     """
 
-    # get session ticket
     auth_client = Authentication()
     content_endpoint = '/rest/search/' + 'current'
-    page_number = 0
-    umls_results = []
-    result = ''
+    page_number, umls_results, result = 0, [], ''
 
     while result != 'NO RESULTS':
-        tries = 0
-        r = ''
+        tries, r = 0, ''
         while r == '' and tries != 20:
             try:
-                # generate a new service ticket for each page if needed
                 ticket = auth_client.getst(auth_client.gettgt())
                 page_number += 1
                 query = {'string': code, 'ticket': ticket, 'pageNumber': page_number, 'inputType': 'sourceUi',
                          'searchType': 'exact', 'includeObsolete': 'true', 'includeSuppressible': 'true'}
-
                 r = requests.get('https://uts-ws.nlm.nih.gov' + content_endpoint, params=query)
                 r.encoding = 'utf-8'
-
-            except requests.exceptions.Timeout as e:
+            except (requests.exceptions.Timeout, requests.exceptions.ConnectionError) as e:
                 print('Timeout ' + str(e))
-                print('\n')
-
-                # sleep and then try again
                 sleep(10)
                 tries += 1
-
-                # generate a new service ticket for each page if needed
-                ticket = auth_client.getst(auth_client.gettgt())
-                page_number += 1
-                query = {'string': code, 'ticket': ticket, 'pageNumber': page_number, 'inputType': 'sourceUi',
-                         'searchType': 'exact', 'includeObsolete': 'true', 'includeSuppressible': 'true'}
-
-                r = requests.get('https://uts-ws.nlm.nih.gov' + content_endpoint, params=query)
-                r.encoding = 'utf-8'
-
-            except requests.exceptions.ConnectionError as e:
-                print('ConnectionError ' + str(e))
-                print('\n')
-
-                # sleep and then try again
-                sleep(10)
-                tries += 1
-
-                # generate a new service ticket for each page if needed
                 ticket = auth_client.getst(auth_client.gettgt())
                 page_number += 1
                 query = {'string': code, 'ticket': ticket, 'pageNumber': page_number, 'inputType': 'sourceUi',
                          'searchType': 'exact', 'includeObsolete': 'true', 'includeSuppressible': 'true'}
                 r = requests.get('https://uts-ws.nlm.nih.gov' + content_endpoint, params=query)
                 r.encoding = 'utf-8'
-
             except requests.exceptions.RequestException as e:
                 print(e)
                 print('\n')
                 sys.exit(1)
 
-        # load results
         items = json.loads(r.text)
         json_data = items['result']
 
-        # test if results were found
         if json_data['results'][0]['name'] == 'NO RESULTS':
             result = json_data['results'][0]['name']
         else:
@@ -158,57 +119,45 @@ def code_search(code):
 
     out_message = 'UMLS API returned: %d' % len(umls_results) + ' result(s) for ' + str(code)
 
-    # print('\n')
-    # print('=' * len(out_message))
-    # print(out_message)
-    # print('=' * len(out_message))
-    # print('\n')
+    print('\n')
+    print('=' * len(out_message))
+    print(out_message)
+    print('=' * len(out_message))
+    print('\n')
 
     return umls_results
 
 
-def cui_search(cui):
+def cui_search(cui: str):
     """This function takes a string containing the UMLS REST API URI, a string containing a UMLS CUI, and a string
     specifying UMLS version to query (default is set to query the most up to date version). Using this information the
     REST API is queried. Function returns a dictionary containing the results for a single query. Additional input
     and output information can be found: https://documentation.uts.nlm.nih.gov/rest/concept/index.html.
 
     Args:
-        cui: a string containing a UMLS CUI
+        cui: A string containing a UMLS CUI.
 
     Returns:
-        dictionary containing the results for a single query
+        A dictionary containing the results for a single query.
     """
 
-    # get session ticket
     auth_client = Authentication()
     tgt = auth_client.gettgt()
     content_endpoint = '/rest/content/' + 'current' + '/CUI/' + str(cui)
     query = {'ticket': auth_client.getst(tgt)}
 
-    # request results and track certain exceptions
-    tries = 0
-    r = ''
+    tries, r = 0, ''
     while r == '' and tries != 20:
-
         try:
             r = requests.get('https://uts-ws.nlm.nih.gov' + content_endpoint, params=query)
             r.encoding = 'utf-8'
-
-        except requests.exceptions.Timeout:
+        except (requests.exceptions.Timeout, requests.exceptions.ConnectionError):
             sleep(10)
             tries += 1
-
-        except requests.exceptions.ConnectionError:
-            sleep(10)
-            tries += 1
-
         except requests.exceptions.RequestException as e:
-            # catastrophic error. bail.
             print(e)
             sys.exit(1)
 
-    # retrieve results
     items = json.loads(r.text)
     json_data = items['result']
 
