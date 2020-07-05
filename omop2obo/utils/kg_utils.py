83161b0b298c9a17f8579df4bd8550f9a6d6b310
callahantiff <callahantiff@gmail.com>
2020-07-05 15:15:14 -0600

    adding utility functions for working with ontologies

---
+++ omop2obo/utils/kg_utils.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Knowledge Graph Utility Functions.
+
+Interacts with OWL Tools API
+* gets_ontology_classes
+* gets_deprecated_ontology_classes
+
+"""
+
+# import needed libraries
+import os
+import os.path
+from rdflib import Graph, URIRef  # type: ignore
+from rdflib.namespace import RDF, OWL  # type: ignore
+import subprocess
+
+from tqdm import tqdm  # type: ignore
+from typing import Set
+
+
+def gets_ontology_classes(graph: Graph) -> Set:
+    """Queries a knowledge graph and returns a list of all owl:Class objects in the graph.
+
+    Args:
+        graph: An rdflib Graph object.
+
+    Returns:
+        class_list: A list of all of the classes in the graph.
+
+    Raises:
+        ValueError: If the query returns zero nodes with type owl:ObjectProperty.
+    """
+
+    print('\nQuerying Knowledge Graph to Obtain all OWL:Class Nodes')
+
+    # find all classes in graph
+    kg_classes = graph.query(
+        """SELECT DISTINCT ?c
+             WHERE {?c rdf:type owl:Class . }
+        """, initNs={'rdf': RDF, 'owl': OWL}
+    )
+
+    # convert results to list of classes
+    class_list = set([res[0] for res in tqdm(kg_classes) if isinstance(res[0], URIRef)])
+
+    if len(class_list) > 0:
+        return class_list
+    else:
+        raise ValueError('ERROR: No classes returned from query.')
+
+
+def gets_deprecated_ontology_classes(graph: Graph) -> Set:
+    """Queries a knowledge graph and returns a list of all deprecated owl:Class objects in the graph.
+
+    Args:
+        graph: An rdflib Graph object.
+
+    Returns:
+        class_list: A list of all of the deprecated OWL classes in the graph.
+
+    Raises:
+        ValueError: If the query returns zero nodes with type owl:Class.
+    """
+
+    print('\nQuerying Knowledge Graph to Obtain all deprecated OWL:Class Nodes')
+
+    # find all classes in graph
+    kg_classes = graph.query(
+        """SELECT DISTINCT ?c
+             WHERE {?c owl:deprecated true . }
+        """, initNs={'owl': OWL}
+    )
+
+    # convert results to list of classes
+    class_list = set([res[0] for res in tqdm(kg_classes) if isinstance(res[0], URIRef)])
+
+    return class_list
+
+
+def gets_ontology_statistics(file_location: str, owltools_location: str = './pkt_kg/libs/owltools') -> None:
+    """Uses the OWL Tools API to generate summary statistics (i.e. counts of axioms, classes, object properties, and
+    individuals).
+
+    Args:
+        file_location: A string that contains the file path and name of an ontology.
+        owltools_location: A string pointing to the location of the owl tools library.
+
+    Returns:
+        None.
+
+    Raises:
+        TypeError: If the file_location is not type str.
+        OSError: If file_location points to a non-existent file.
+        ValueError: If file_location points to an empty file.
+    """
+
+    if not isinstance(file_location, str):
+        raise TypeError('ERROR: file_location must be a string')
+    elif not os.path.exists(file_location):
+        raise OSError('The {} file does not exist!'.format(file_location))
+    elif os.stat(file_location).st_size == 0:
+        raise ValueError('FILE ERROR: input file: {} is empty'.format(file_location))
+    else:
+        output = subprocess.check_output([os.path.abspath(owltools_location), file_location, '--info'])
+
+    # print stats
+    res = output.decode('utf-8').split('\n')[-5:]
+    cls, axs, op, ind = res[0].split(':')[-1], res[3].split(':')[-1], res[2].split(':')[-1], res[1].split(':')[-1]
+    sent = '\nThe knowledge graph contains {0} classes, {1} axioms, {2} object properties, and {3} individuals\n'
+
+    print(sent.format(cls, axs, op, ind))
+
+    return None
---
+++ tests/test_kg_utils.py
@@ -0,0 +1,82 @@
+import glob
+import os
+import os.path
+import unittest
+
+from typing import Set
+from rdflib import Graph
+
+from omop2obo.utils import gets_ontology_statistics, gets_ontology_classes, gets_deprecated_ontology_classes
+
+
+class TestKGUtils(unittest.TestCase):
+    """Class to test knowledge graph utility methods."""
+
+    def setUp(self):
+        # initialize data location
+        current_directory = os.path.dirname(__file__)
+        dir_loc = os.path.join(current_directory, 'data/ontologies')
+        self.dir_loc = os.path.abspath(dir_loc)
+
+        # set some real and fake file name variables
+        self.not_string_filename = [self.dir_loc + '/empty_hp_with_imports.owl']
+        self.not_real_file_name = self.dir_loc + '/sop_with_imports.owl'
+        self.empty_ontology_file_location = self.dir_loc + '/empty_hp_with_imports.owl'
+        self.good_ontology_file_location = self.dir_loc + '/so_with_imports.owl'
+
+        # pointer to owltools
+        dir_loc2 = os.path.join(current_directory, 'utils/owltools')
+        self.owltools_location = os.path.abspath(dir_loc2)
+
+        return None
+
+    def test_gets_ontology_statistics(self):
+        """Tests gets_ontology_statistics method."""
+
+        # test non-string file name
+        self.assertRaises(TypeError, gets_ontology_statistics, self.not_string_filename)
+
+        # test fake file name
+        self.assertRaises(OSError, gets_ontology_statistics, self.not_real_file_name)
+
+        # test empty file
+        self.assertRaises(ValueError, gets_ontology_statistics, self.empty_ontology_file_location)
+
+        # test good file
+        self.assertIsNone(gets_ontology_statistics(self.good_ontology_file_location, self.owltools_location))
+
+        return None
+
+    def test_gets_ontology_classes(self):
+        """Tests the gets_ontology_classes method."""
+
+        # read in ontology
+        graph = Graph()
+        graph.parse(self.good_ontology_file_location)
+
+        # retrieve classes form graph with data
+        classes = gets_ontology_classes(graph)
+
+        self.assertIsInstance(classes, Set)
+        self.assertEqual(2573, len(classes))
+
+        # retrieve classes form graph with no data
+        no_data_graph = Graph()
+        self.assertRaises(ValueError, gets_ontology_classes, no_data_graph)
+
+        return None
+
+    def test_gets_deprecated_ontology_classes(self):
+        """Tests the gets_deprecated_ontology_classes method."""
+
+        # read in ontology
+        graph = Graph()
+        graph.parse(self.good_ontology_file_location)
+
+        # retrieve classes form graph with data
+        classes = gets_deprecated_ontology_classes(graph)
+
+        self.assertIsInstance(classes, Set)
+        self.assertEqual(336, len(classes))
+
+        return None
