45ccfbf9c6aba7cf80ce97f2492a0982673256ac
callahantiff <callahantiff@gmail.com>
2020-07-05 22:19:46 -0600

    cleaning up script

--- omop2obo/ontology_explorer.py
+++ omop2obo/ontology_explorer.py
@@ -4,6 +4,7 @@
 
 # import needed libraries
 import glob
+import os
 import pickle
 
 from datetime import datetime
@@ -22,12 +23,24 @@ class OntologyInfoExtractor(object):
 
     Attributes:
         graph: An rdflib graph object.
+        ont_dictionary: A dictionary, where keys are a string aliasing an ontology and values are a string pointing
+            the local location where the ontology was downloaded.
+        ont_directory: A string containing the filepath to the ontology data directory.
     """
 
-    def __init__(self) -> None:
+    def __init__(self, ontology_directory: str) -> None:
         self.graph: Graph = Graph()
 
-        #
+        # check for ontology data
+        if not os.path.exists(ontology_directory):
+            raise OSError("Can't find 'ontologies/' directory, this directory is a required input")
+        elif len(glob.glob(ontology_directory + '/*.owl')) == 0:
+            raise TypeError('The ontologies directory is empty')
+        else:
+            dict_files = glob.glob(ontology_directory + '/*.owl')
+            self.ont_dictionary: Dict[str] = {x.split('/')[-1].split('_')[0]: x for x in dict_files
+                                              if os.stat(x).st_size != 0}
+            self.ont_directory: str = ontology_directory
 
     def get_ontology_information(self, ont_id: str, codes: List = None) -> Dict:
         """Function queries an RDF graph and returns labels, definitions, dbXRefs, and synonyms for all
@@ -55,46 +68,47 @@ class OntologyInfoExtractor(object):
                                              Literal('true',
                                                      datatype=URIRef('http://www.w3.org/2001/XMLSchema#boolean'))))
 
-        for cls in tqdm(class_ids):
-            if ont_id in str(cls) and cls not in class_dep:
+        # filter classes to only include those that are in the ont_id namespace and are not deprecated
+        filtered_classes = [x for x in list(set(class_ids) - set(class_dep)) if ont_id.lower() in str(x).lower()]
 
-                # labels
-                for x in list(self.graph.objects(cls, RDFS.label)):
-                    res['label'][str(x).encode('ascii', 'ignore').lower().decode('utf-8')] = str(cls)
+        for cls in tqdm(filtered_classes):
+            # labels
+            for x in list(self.graph.objects(cls, RDFS.label)):
+                res['label'][str(x).encode('ascii', 'ignore').lower().decode('utf-8')] = str(cls)
 
-                # definitions
-                for x in list(self.graph.objects(cls, URIRef(obo + 'IAO_0000115'))):
-                    res['definition'][str(x).encode('ascii', 'ignore').lower().decode('utf-8')] = str(cls)
+            # definitions
+            for x in list(self.graph.objects(cls, URIRef(obo + 'IAO_0000115'))):
+                res['definition'][str(x).encode('ascii', 'ignore').lower().decode('utf-8')] = str(cls)
 
-                # dbXRef
-                for x in list(self.graph.objects(cls, URIRef(oboinowl + 'hasDbXref'))):
-                    if codes:
-                        if any(i for i in codes if i in str(x)):
-                            res['dbxref'][str(x)] = str(cls)
-                    else:
+            # dbxref
+            for x in list(self.graph.objects(cls, URIRef(oboinowl + 'hasDbXref'))):
+                if codes:
+                    if any(i for i in codes if i in str(x)):
                         res['dbxref'][str(x)] = str(cls)
+                else:
+                    res['dbxref'][str(x)] = str(cls)
 
-                # synonyms
-                syns = [str(x).encode('ascii', 'ignore').lower().decode('utf-8')
-                        for x in list(self.graph.objects(cls, URIRef(oboinowl + 'hasSynonym')))]
-                syns += [str(x).encode('ascii', 'ignore').lower().decode('utf-8')
-                         for x in list(self.graph.objects(cls, URIRef(oboinowl + 'hasExactSynonym')))]
-                syns += [str(x).encode('ascii', 'ignore').lower().decode('utf-8')
-                         for x in list(self.graph.objects(cls, URIRef(oboinowl + '#hasBroadSynonym')))]
-                syns += [str(x).encode('ascii', 'ignore').lower().decode('utf-8')
-                         for x in list(self.graph.objects(cls, URIRef(oboinowl + 'hasNarrowSynonym')))]
-                syns += [str(x).encode('ascii', 'ignore').lower().decode('utf-8')
-                         for x in list(self.graph.objects(cls, URIRef(oboinowl + 'hasRelatedSynonym')))]
+            # synonyms
+            syns = [str(x).encode('ascii', 'ignore').lower().decode('utf-8')
+                    for x in list(self.graph.objects(cls, URIRef(oboinowl + 'hasSynonym')))]
+            syns += [str(x).encode('ascii', 'ignore').lower().decode('utf-8')
+                     for x in list(self.graph.objects(cls, URIRef(oboinowl + 'hasExactSynonym')))]
+            syns += [str(x).encode('ascii', 'ignore').lower().decode('utf-8')
+                     for x in list(self.graph.objects(cls, URIRef(oboinowl + '#hasBroadSynonym')))]
+            syns += [str(x).encode('ascii', 'ignore').lower().decode('utf-8')
+                     for x in list(self.graph.objects(cls, URIRef(oboinowl + 'hasNarrowSynonym')))]
+            syns += [str(x).encode('ascii', 'ignore').lower().decode('utf-8')
+                     for x in list(self.graph.objects(cls, URIRef(oboinowl + 'hasRelatedSynonym')))]
 
-                for x in syns:
-                    res['synonyms'][x] = str(cls)
+            for x in syns:
+                res['synonyms'][x] = str(cls)
 
         finish = datetime.now()
         print("Finished processing query: {}".format(finish))
 
         return res
 
-    def ontology_processor(self, ont_info_dictionary: Dict) -> None:
+    def ontology_processor(self) -> None:
         """Using different information from the user, this function retrieves all class labels, definitions,
         synonyms, and database cross-references (dbXref). The function expects a dictionary as input where the keys are
         short nick-names or OBO abbreviations for ontologies and the values are lists, where the first item is a string
@@ -103,14 +117,11 @@ class OntologyInfoExtractor(object):
 
             {'CHEBI': ['resources/ontologies/chebi_without_imports.owl', ['DrugBank', 'ChEMBL', 'UniProt']]}
 
-        Args:
-            ont_info_dictionary: A dictionary. Detail on this is provided in the function description.
-
         Returns:
             None.
         """
 
-        for ont in ont_info_dictionary.items():
+        for ont in self.ont_dictionary.items():
             print('\n****** Processing Ontology: {0}'.format(ont[0]))
 
             # create graph
@@ -124,34 +135,32 @@ class OntologyInfoExtractor(object):
 
         return None
 
-    # @staticmethod
-    # def ontology_loader(ontologies: List) -> Dict:
-    #     """Function takes a list of file paths to pickled data, loads the data, and then saves each file as a dictionary
-    #     entry.
-    #
-    #     Args:
-    #         ontologies: A list of strings representing ontologies.
-    #
-    #     Returns:
-    #         A dictionary where each key is a file name and each value is a dictionary.
-    #
-    #     Raises:
-    #         ValueError: If the provided ontology name does not match any downloaded ontology files.
-    #         ValueError: If the number of dictionary entries does not equal the number of files in the files list.
-    #     """
-    #
-    #     # find files that match user input
-    #     ont_files = [(e, glob.glob('resources/ontologies/' + str(e.lower()) + '*.pickle')[0]) for e in ontologies]
-    #
-    #     if len(ont_files) == 0:
-    #         raise ValueError('Unable to find files that include that ontology name')
-    #     else:
-    #         ontology_data = {}
-    #         for ont, f in tqdm(ont_files):
-    #             with open(f, 'rb') as _file:
-    #                 ontology_data[ont] = pickle.load(_file)
-    #
-    #         if len(ont_files) != len(ontology_data):
-    #             raise ValueError('Unable to load all of files referenced in the file path')
-    #         else:
-    #             return ontology_data
+    def ontology_loader(self) -> Dict:
+        """Function takes a list of file paths to pickled data, loads the data, and then saves each file as a dictionary
+        entry.
+
+        Returns:
+            A dictionary where each key is a file name and each value is a dictionary.
+
+        Raises:
+            ValueError: If the provided ontology name does not match any downloaded ontology files.
+            ValueError: If the number of dictionary entries does not equal the number of files in the files list.
+        """
+
+        # find files that match user input
+        ont_files = [(e, glob.glob(self.ont_directory + '/' + str(e.lower()) + '*.pickle')[0])
+                     for e in self.ont_dictionary.keys() if
+                     len(glob.glob(self.ont_directory + '/' + str(e.lower()) + '*.pickle')) != 0]
+
+        if len(ont_files) == 0:
+            raise ValueError('Unable to find files that include that ontology name')
+        else:
+            ontology_data = {}
+            for ont, f in tqdm(ont_files):
+                with open(f, 'rb') as _file:
+                    ontology_data[ont] = pickle.load(_file)
+
+            if len(ont_files) != len(ontology_data):
+                raise ValueError('Unable to load all of files referenced in the file path')
+            else:
+                return ontology_data
