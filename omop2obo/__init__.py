cce1f7820cc57bbfb1178cde071c9120902107aa
callahantiff <callahantiff@gmail.com>
2020-07-05 19:29:53 -0600

    updating init content

--- omop2obo/__init__.py
+++ omop2obo/__init__.py
@@ -4,24 +4,20 @@
 
 """ A Library for mapping OMOP concepts to OBO
 
-Precision medicine requires timely synthesis of clinical and genomic data. Despite large-scale biobanking efforts,
-most electronic health records (EHRs) do not systematically integrate nor have the infrastructure to incorporate
-genomic data. Common data models (CDMs) have solved many of the challenges of utilizing EHR data, yet they do not
-include resources needed to integrate or interpret clinical and genomic data. Biomedical ontologies provide accurate
-and semantically computable representations of biological knowledge. Aligning patient data to open biomedical ontologies
-(OBO) requires manual and/or semi-automated curation and domain expertise, limiting existing efforts to specific
-diseases and clinical data.
+OMOP2OBO is the first health system-wide, disease-agnostic mappings between standardized clinical terminologies in
+the [Observational Medical Outcomes Partnership (OMOP) common data model and several Open Biomedical Ontologies (OBO)
+foundry ontologies. These mappings were validated by domain experts and their coverage was examined in several health
+systems.
 
-We introduce OMOP2OBO - the first health system-wide, disease-agnostic mappings between standardized clinical
-terminologies in the Observational Medical Outcomes Partnership (OMOP) CDM and several OBO foundry ontologies. These
-mappings were validated by domain experts and their coverage was examined in several health systems.
-
-There is one way to run PheKnowLator (NEEDS TO BE FINISHED):
+There is one way to run PheKnowLator:
   1. Command line via argparse (Main.py)
 """
 
 __all__ = [
-    'OntologyDownloader'
+    'OntologyDownloader',
+
+    'OntologyInfoExtractor'
 ]
 
 from omop2obo.ontology_downloader import OntologyDownloader
+from omop2obo.ontology_explorer import OntologyInfoExtractor
