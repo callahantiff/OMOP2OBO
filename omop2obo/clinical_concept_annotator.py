6c41fbdb985a329cdb77f921e69fd7502a32dfd5
callahantiff <callahantiff@gmail.com>
2020-07-07 16:59:19 -0600

    adding new method

--- omop2obo/clinical_concept_annotator.py
+++ omop2obo/clinical_concept_annotator.py
@@ -8,13 +8,17 @@ import pandas as pd  # type: ignore
 
 from abc import ABCMeta, abstractmethod
 from pandas import errors
+from tqdm import tqdm  # type: ignore
 from typing import Dict, Optional
-
-from omop2obo.utils.umls_api import *
 
 
 class ConceptAnnotator(object):
-    """An annotator to map clinical codes to ontology terms.
+    """An annotator to map clinical codes to ontology terms. This workflow consists of four steps that are performed
+    on data from a single clinical domain:
+        1 - UMLS CUI and Semantic Type Annotation
+        2 - Ontology DbXRef Mapping
+        3 - Exact String Mapping to concept labels and/or synonyms
+        4 - Similarity distance mapping
 
     Attributes:
         clinical_data: A Pandas DataFrame containing clinical data.
@@ -22,23 +26,27 @@ class ConceptAnnotator(object):
             (e.g. "hp", "mondo"), inner keys are data types (e.g. "label", "definition", "dbxref", and "synonyms").
             For each inner key, there is a third dictionary keyed by a string of that item type and with values that
             are the ontology URI for that string type.
-        umls_data: A Pandas DataFrame containing UMLS CUI data.
+        umls_cui_data: A Pandas DataFrame containing UMLS CUI data from MRCONSO.RRF.
+        umls_tui_data: A Pandas DataFrame containing UMLS CUI data from MRSTY.RRF.
 
     Raises:
         TypeError:
             If clinical_file is not type str or if clinical_file is empty.
             If ontology_dictionary is not type dict.
-            If umls_file is not type str or if umls_mrconso is empty.
+            If umls_mrconso_file is not type str or if umls_mrconso_file is empty.
+            If umls_mrsty_file is not type str or if umls_mrsty_file is empty.
         OSError:
             If the clinical_file does not exist.
-            If umls_file does not exist.
+            If umls_mrconso_file does not exist.
+            If umls_mrsty_file does not exist.
     """
 
     __metaclass__ = ABCMeta
 
-    def __init__(self, clinical_file: str, ontology_dictionary: Dict, umls_file: str = None) -> None:
+    def __init__(self, clinical_file: str, ontology_dictionary: Dict, umls_mrconso_file: str = None,
+                 umls_mrsty_file: str = None) -> None:
 
-        # check clinical_file
+        # clinical_file
         if not isinstance(clinical_file, str):
             raise TypeError('clinical_file must be type str.')
         elif not os.path.exists(clinical_file):
@@ -48,59 +56,67 @@ class ConceptAnnotator(object):
         else:
             print('**Loading {} **'.format(clinical_file))
             try:
-                self.clinical_data: pd.DataFrame = pd.read_csv(clinical_file, header=0, sep=',',
-                                                               low_memory=False).astype(str)
+                self.clinical_data: pd.DataFrame = pd.read_csv(clinical_file, header=0, low_memory=False).astype(str)
             except pd.errors.ParserError:
-                self.clinical_data = pd.read_csv(clinical_file, header=0, sep='\t',
-                                                 low_memory=False).astype(str)
+                self.clinical_data = pd.read_csv(clinical_file, header=0, sep='\t', low_memory=False).astype(str)
 
         # check ontology_dictionary
-        if not isinstance(ontology_dictionary, Dict):
-            raise TypeError('ontology_dictionary must be type dict.')
-        else:
-            self.ontology_dictionary: Dict = ontology_dictionary
+        if not isinstance(ontology_dictionary, Dict): raise TypeError('ontology_dictionary must be type dict.')
+        else: self.ontology_dictionary: Dict = ontology_dictionary
 
         # check for UMLS MRCONSO file
-        if not umls_file:
-            self.umls_data: Optional[pd.DataFrame] = None
+        if not umls_mrconso_file: self.umls_data: Optional[pd.DataFrame] = None
         else:
-            if not isinstance(umls_file, str):
-                raise TypeError('umls_mrconso must be type str.')
-            elif not os.path.exists(umls_file):
-                raise OSError('The {} file does not exist!'.format(umls_file))
-            elif os.stat(umls_file).st_size == 0:
-                raise TypeError('Input file: {} is empty'.format(umls_file))
+            if not isinstance(umls_mrconso_file, str):
+                raise TypeError('umls_mrconso_file must be type str.')
+            elif not os.path.exists(umls_mrconso_file):
+                raise OSError('The {} file does not exist!'.format(umls_mrconso_file))
+            elif os.stat(umls_mrconso_file).st_size == 0:
+                raise TypeError('Input file: {} is empty'.format(umls_mrconso_file))
             else:
                 print('**Loading UMLS MRCONSO Data **')
                 headers = ['CUI', 'SAB', 'CODE']
-                self.umls_data = pd.read_csv(umls_file, header=None, sep='|', names=headers, low_memory=False,
-                                             usecols=[0, 11, 13]).drop_duplicates().astype(str)
-                self.umls_data = self.umls_data[self.umls_data.CODE != 'NOCODE'].drop_duplicates()  # remove 'NOCODE'
+                self.umls_cui_data = pd.read_csv(umls_mrconso_file, header=None, sep='|', names=headers,
+                                                 low_memory=False, usecols=[0, 11, 13]).drop_duplicates().astype(str)
+                self.umls_cui_data = self.umls_cui_data[self.umls_cui_data.CODE != 'NOCODE'].drop_duplicates()
 
-    def umls_annotator(self, code_type: str) -> Optional[pd.DataFrame]:
-        """Method maps concepts in the map_column in a clinical data file to UMLS concepts in the umls_mrconso Pandas
-        DataFrame.
+        # check for UMLS MRSTY file
+        if not umls_mrsty_file: self.umls_tui_data: Optional[pd.DataFrame] = None
+        else:
+            if not isinstance(umls_mrsty_file, str):
+                raise TypeError('umls_mrsty_file must be type str.')
+            elif not os.path.exists(umls_mrsty_file):
+                raise OSError('The {} file does not exist!'.format(umls_mrsty_file))
+            elif os.stat(umls_mrsty_file).st_size == 0:
+                raise TypeError('Input file: {} is empty'.format(umls_mrsty_file))
+            else:
+                print('**Loading UMLS MRSTY Data **')
+                headers = ['CUI', 'STY']
+                self.umls_tui_data = pd.read_csv(umls_mrsty_file, header=None, sep='|', names=headers,
+                                                 low_memory=False, usecols=[0, 3]).drop_duplicates().astype(str)
 
-        Args:
-            code_type: A string containing the name of a clinical vocabulary to use when filtering the UMLS (
+    def umls_cui_annotator(self) -> Optional[pd.DataFrame]:
+        """Method maps concepts in a clinical data file to UMLS concepts and semantic types from the umls_cui_data
+        and umls_tui_data Pandas Data Frames.
 
         Returns:
-            None.
-
+           umls_cui_semtype: A Pandas DataFrame containing clinical concept ids and source codes as well as UMLS
+            CUIs, source codes, and semantic types.
         """
 
-        # subset DataFrames to only include columns needed for merge
-        if self.umls_data:
-            umls_cui = self.umls_data[self.umls_data.apply(lambda x: code_type in x['SAB'], axis=1)].drop_duplicates()
-            clinical_ids = self.clinical_data[['CONCEPT_ID', 'CONCEPT_SOURCE_CODE']].drop_duplicates()
+        if self.umls_cui_data is not None:
+            # umls_cui = self.umls_data[self.umls_data.apply(lambda x: code_type in x['SAB'], axis=1)].drop_duplicates()
+            clin_ids = self.clinical_data[['CONCEPT_ID', 'CONCEPT_SOURCE_CODE']].drop_duplicates()
 
-            # merge reduced concepts
-            umls_merged = clinical_ids.merge(umls_cui, how='left', left_on='CONCEPT_SOURCE_CODE', right_on='CODE')
+            # merge reduced clinical concepts with umls concepts
+            umls_cui = clin_ids.merge(self.umls_cui_data, how='inner', left_on='CONCEPT_SOURCE_CODE', right_on='CODE')
+            umls_cui_semtype = umls_cui.merge(self.umls_tui_data, how='left', on='CUI').drop_duplicates()
 
-            return umls_merged[['CONCEPT_ID', 'CONCEPT_SOURCE_CODE', 'CUI']].drop_duplicates()
+            # update column names
+            updated_cols = ['CONCEPT_ID', 'CONCEPT_SOURCE_CODE', 'UMLS_CUI', 'UMLS_SAB', 'UMLS_CODE', 'UMLS_SEM_TYPE']
+            umls_cui_semtype.columns = updated_cols
 
-        else:
-            return None
+            return umls_cui_semtype
 
     @abstractmethod
     def gets_clinical_domain(self) -> str:
--- tests/test_clinical_concept_annotator.py
+++ tests/test_clinical_concept_annotator.py
@@ -23,17 +23,18 @@ class TestConceptAnnotator(TestCase):
 
         # create some fake ontology data
         with open(self.dir_loc + '/condition_occurrence_ontology_dictionary.pickle', 'rb') as handle:
-            self.ont_dictionary = pickle.load(handle)
+            self.ont_dict = pickle.load(handle)
         handle.close()
 
         # link to fake clinical data
         self.clinical_file = self.clinical_directory + '/sample_omop_condition_occurrence_data.csv'
 
-        # link to fake clinical data - PLEASE DO NOT REDISTRIBUTE THIS FILE
-        self.umls_mrconso = self.mapping_directory + '/MRCONSO_FAKE.RRF'
+        # link to stubbed UMLS data - CUIs have been scrambled
+        self.umls_cui = self.mapping_directory + '/MRCONSO_FAKE.RRF'
+        self.umls_tui = self.mapping_directory + '/MRSTY_FAKE.RRF'
 
         # initialize the class
-        self.annotator = ConceptAnnotator(self.clinical_file, self.ont_dictionary, self.umls_mrconso)
+        self.annotator = ConceptAnnotator(self.clinical_file, self.ont_dict, self.umls_cui, self.umls_tui)
 
         return None
 
@@ -42,15 +43,15 @@ class TestConceptAnnotator(TestCase):
 
         # test if file is not type string
         clinical_file_type = 1234
-        self.assertRaises(TypeError, ConceptAnnotator, clinical_file_type, self.ont_dictionary, self.umls_mrconso)
+        self.assertRaises(TypeError, ConceptAnnotator, clinical_file_type, self.ont_dict, self.umls_cui, self.umls_tui)
 
         # test if file exists
         clinical_file_exists = self.clinical_directory + '/sample_omop_condition_occurrence.csv'
-        self.assertRaises(OSError, ConceptAnnotator, clinical_file_exists, self.ont_dictionary, self.umls_mrconso)
+        self.assertRaises(OSError, ConceptAnnotator, clinical_file_exists, self.ont_dict, self.umls_cui, self.umls_tui)
 
         # test if file is empty
         clinical_file_empty = self.clinical_directory + '/sample_omop_condition_occurrence_data_empty.csv'
-        self.assertRaises(TypeError, ConceptAnnotator, clinical_file_empty, self.ont_dictionary, self.umls_mrconso)
+        self.assertRaises(TypeError, ConceptAnnotator, clinical_file_empty, self.ont_dict, self.umls_cui, self.umls_tui)
 
         return None
 
@@ -58,23 +59,51 @@ class TestConceptAnnotator(TestCase):
         """Test class initialization for ontologies dictionary input argument."""
 
         # check if ontology_dict is not type dict
-        self.assertRaises(TypeError, ConceptAnnotator, self.clinical_file, 12, self.umls_mrconso)
+        self.assertRaises(TypeError, ConceptAnnotator, self.clinical_file, 12, self.umls_cui, self.umls_tui)
 
         return None
 
-    def test_initialization_mrconso(self):
+    def test_initialization_umls_mrconso(self):
         """Test class initialization for the umls_mrconso data input argument."""
 
         # test if file is not type string
         mrconso_type = 1234
-        self.assertRaises(TypeError, ConceptAnnotator, self.clinical_file, self.ont_dictionary, mrconso_type)
+        self.assertRaises(TypeError, ConceptAnnotator, self.clinical_file, self.ont_dict, mrconso_type, self.umls_tui)
 
         # test if file exists
         mrconso_exists = self.mapping_directory + '/MRCONSO.RRF'
-        self.assertRaises(OSError, ConceptAnnotator, self.clinical_file, self.ont_dictionary, mrconso_exists)
+        self.assertRaises(OSError, ConceptAnnotator, self.clinical_file, self.ont_dict, mrconso_exists, self.umls_tui)
 
         # test if file is empty
         mrconso_empty = self.mapping_directory + '/MRCONSO_EMPTY.RRF'
-        self.assertRaises(TypeError, ConceptAnnotator, self.clinical_file, self.ont_dictionary, mrconso_empty)
+        self.assertRaises(TypeError, ConceptAnnotator, self.clinical_file, self.ont_dict, mrconso_empty, self.umls_tui)
+
+        return None
+
+    def test_initialization_umls_mrsty(self):
+        """Test class initialization for the umls_srdef data input argument."""
+
+        # test if file is not type string
+        mrsty_type = 1234
+        self.assertRaises(TypeError, ConceptAnnotator, self.clinical_file, self.ont_dict, self.umls_cui, mrsty_type)
+
+        # test if file exists
+        mrsty_exists = self.mapping_directory + '/MRSTY.RRF'
+        self.assertRaises(OSError, ConceptAnnotator, self.clinical_file, self.ont_dict, self.umls_cui, mrsty_exists)
+
+        # test if file is empty
+        mrsty_empty = self.mapping_directory + '/MRSTY_EMPTY.RRF'
+        self.assertRaises(TypeError, ConceptAnnotator, self.clinical_file, self.ont_dict, self.umls_cui, mrsty_empty)
+
+        return None
+
+    def test_umls_cui_annotator(self):
+        """Test the umls_cui_annotation method."""
+
+        # run the method and verify the output
+        umls_annotated_data = self.annotator.umls_cui_annotator()
+        self.assertTrue(len(umls_annotated_data) == 1)
+        self.assertTrue(len(umls_annotated_data.columns) == 6)
+        self.assertEqual(umls_annotated_data.at[0, 'UMLS_SEM_TYPE'], 'Pharmacologic Substance')
 
         return None
