a96e180d25f834609e7b64c053b70c6eb36d7467
callahantiff <callahantiff@gmail.com>
2020-07-07 10:18:09 -0600

    missing comma ... again

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def find_version(*file_paths):
 
 __version__ = find_version("omop2obo", "__version__.py")
 
-test_deps =[
+test_deps = [
     "pytest",
     "pytest-cov",
     "coveralls",
@@ -59,16 +59,17 @@ setup(
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     tests_require=test_deps,
     # Add here the package dependencies
-    install_requires=['click',
-                      'google-api-core==1.17.0',
-                      'google-auth==1.14.1',
-                      'google-auth-oauthlib==0.4.1'
-                      'google-cloud==0.34.0',
-                      'google-cloud-core==1.3.0',
-                      'google-cloud-storage==1.28.0',
-                      'lxml',
-                      'oauth2client==4.1.3',
-                      'pandas==1.0.3',
-                      'rdflib==5.0.0'],
+    install_requires=[
+        'click',
+        'google-api-core==1.17.0',
+        'google-auth==1.14.1',
+        'google-auth-oauthlib==0.4.1',
+        'google-cloud==0.34.0',
+        'google-cloud-core==1.3.0',
+        'google-cloud-storage==1.28.0',
+        'lxml',
+        'oauth2client==4.1.3',
+        'pandas==1.0.3',
+        'rdflib==5.0.0'],
     extras_require=extras,
 )
