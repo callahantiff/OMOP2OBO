dfe32ce5fc54a5fcf23ba8c372b615f7aa85568e
callahantiff <callahantiff@gmail.com>
2020-07-06 21:04:11 -0600

    adding umls utils function

--- omop2obo/utils/__init__.py
+++ omop2obo/utils/__init__.py
@@ -4,8 +4,9 @@
 
 from .data_utils import *
 from .ontology_utils import *
+from .umls_api import cui_search
 
 
 __all__ = ['url_download', 'ftp_url_download', 'gzipped_ftp_url_download', 'zipped_url_download',
            'gzipped_url_download', 'data_downloader', 'gets_ontology_statistics', 'gets_ontology_classes',
-           'gets_deprecated_ontology_classes']
+           'gets_deprecated_ontology_classes', 'cui_search']
