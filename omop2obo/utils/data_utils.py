425c72a80cc6622e065681cf5ffb3f50017e2553
callahantiff <callahantiff@gmail.com>
2020-07-11 13:04:46 -0600

    updating script with revised dowloading assumptions

--- omop2obo/utils/data_utils.py
+++ omop2obo/utils/data_utils.py
@@ -4,14 +4,6 @@
 """
 Data PreProcessing Utility Functions.
 
-Downloads Data from a Url
-* url_download
-* ftp_url_download
-* gzipped_ftp_url_download
-* zipped_url_download
-* gzipped_url_download
-* data_downloader
-
 Pandas DataFrame manipulations
 * data_frame_subsetter
 * data_frame_supersetter
@@ -24,224 +16,16 @@ Dictionary manipulations
 """
 
 # import needed libraries
-import ftplib
-import gzip
-import os
 import pandas as pd  # type: ignore
-import re
-import requests
-import shutil
-import urllib3  # type: ignore
 
-from contextlib import closing
 from functools import reduce
-from io import BytesIO
 from more_itertools import unique_everseen
 from typing import Dict, List  # type: ignore
-from urllib.request import urlopen
-from zipfile import ZipFile
 
 # ENVIRONMENT WARNINGS
 # WARNING 1 - Pandas: disable chained assignment warning rationale:
 # https://stackoverflow.com/questions/20625582/how-to-deal-with-settingwithcopywarning-in-pandas
 pd.options.mode.chained_assignment = None
-
-# WARNING 2 - urllib3: disable insecure request warning
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
-
-def url_download(url: str, write_location: str, filename: str) -> None:
-    """Downloads a file from a URL.
-
-    Args:
-        url: A string that points to the location of a temp mapping file that needs to be processed.
-        write_location: A string that points to a file directory.
-        filename: A string containing a filepath for where to write data to.
-
-    Returns:
-        None.
-
-    Raises:
-        HTTPError: If the response returns a status code other than 200.
-    """
-
-    print('Downloading Data from {}'.format(url))
-    r = requests.get(url, allow_redirects=True, verify=False)
-
-    if r.ok is False:
-        raise requests.HTTPError('{status}: Could not downloaded data from {url}'.format(status=r.status_code, url=url))
-    else:
-        data = None
-        if 'Content-Length' in r.headers:
-            while r.ok and int(r.headers['Content-Length']) < 1000:
-                r = requests.get(url, allow_redirects=True, verify=False)
-            data = r.content
-        else:
-            if len(r.content) > 10:
-                data = r.content
-
-        # download and save data
-        if data:
-            with open(write_location + '{filename}'.format(filename=filename), 'wb') as outfile:
-                outfile.write(data)
-            outfile.close()
-
-    return None
-
-
-def ftp_url_download(url: str, write_location: str, filename: str) -> None:
-    """Downloads a file from an ftp server.
-
-    Args:
-        url: A string that points to the location of a temp mapping file that needs to be processed.
-        write_location: A string that points to a file directory.
-        filename: A string containing a filepath for where to write data to.
-
-    Returns:
-        None.
-    """
-
-    print('Downloading Data from FTP Server: {}'.format(url))
-
-    with closing(urlopen(url)) as downloaded_data:
-        with open(write_location + '{filename}'.format(filename=filename), 'wb') as outfile:
-            shutil.copyfileobj(downloaded_data, outfile)
-        outfile.close()
-
-    return None
-
-
-def gzipped_ftp_url_download(url: str, write_location: str, filename: str) -> None:
-    """Downloads a gzipped file from an ftp server.
-
-    Args:
-        url: A string that points to the location of a temp mapping file that needs to be processed.
-        write_location: A string that points to a file directory.
-        filename: A string containing a filepath for where to write data to.
-
-    Returns:
-        None.
-    """
-
-    # get ftp server info
-    server = url.replace('ftp://', '').split('/')[0]
-    directory = '/'.join(url.replace('ftp://', '').split('/')[1:-1])
-    file = url.replace('ftp://', '').split('/')[-1]
-    write_loc = write_location + '{filename}'.format(filename=file)
-
-    # download ftp gzipped file
-    print('Downloading Gzipped data from FTP Server: {}'.format(url))
-    with closing(ftplib.FTP(server)) as ftp, open(write_loc, 'wb') as fid:
-        ftp.login()
-        ftp.cwd(directory)
-        ftp.retrbinary('RETR {}'.format(file), fid.write)
-
-    # read in gzipped file,uncompress, and write to directory
-    print('Decompressing and Writing Gzipped Data to File')
-    with gzip.open(write_loc, 'rb') as fid_in:
-        with open(write_loc.replace('.gz', ''), 'wb') as file_loc:
-            file_loc.write(fid_in.read())
-
-    # change filename and remove gzipped and original files
-    if filename != '':
-        os.rename(re.sub('.gz|.zip', '', write_loc), write_location + filename)
-
-    # remove compressed file
-    os.remove(write_loc)
-
-    return None
-
-
-def zipped_url_download(url: str, write_location: str, filename: str = '') -> None:
-    """Downloads a zipped file from a URL.
-
-    Args:
-        url: A string that points to the location of a temp mapping file that needs to be processed.
-        write_location: A string that points to a file directory.
-        filename: A string containing a filepath for where to write data to.
-
-    Returns:
-        None.
-
-    Raises:
-        HTTPError: If the response returns a status code other than 200.
-    """
-
-    print('Downloading Zipped Data from {}'.format(url))
-    r = requests.get(url, allow_redirects=True)
-
-    if r.ok is False:
-        raise requests.HTTPError('{status}: Could not downloaded data from {url}'.format(status=r.status_code, url=url))
-    else:
-        with r as zip_data:
-            with ZipFile(BytesIO(zip_data.content)) as zip_file:
-                zip_file.extractall(write_location[:-1])
-        zip_data.close()
-
-        # change filename
-        if filename != '':
-            os.rename(write_location + re.sub('.gz|.zip', '', url.split('/')[-1]), write_location + filename)
-
-    return None
-
-
-def gzipped_url_download(url: str, write_location: str, filename: str) -> None:
-    """Downloads a gzipped file from a URL.
-
-    Args:
-        url: A string that points to the location of a temp mapping file that needs to be processed.
-        write_location: A string that points to a file directory.
-        filename: A string containing a filepath for where to write data to.
-
-    Returns:
-        None.
-
-    Raises:
-        HTTPError: If the response returns a status code other than 200.
-    """
-
-    print('Downloading Gzipped Data from {}'.format(url))
-    r = requests.get(url, allow_redirects=True, verify=False)
-
-    if r.ok is False:
-        raise requests.HTTPError('{status}: Could not downloaded data from {url}'.format(status=r.status_code, url=url))
-    else:
-        with open(write_location + '{filename}'.format(filename=filename), 'wb') as outfile:
-            outfile.write(gzip.decompress(r.content))
-        outfile.close()
-
-    return None
-
-
-def data_downloader(url: str, write_location: str, filename: str = '') -> None:
-    """Downloads data from a URL and saves the file to the `/resources/processed_data/unprocessed_data' directory.
-
-    Args:
-        url: A string that points to the location of a temp mapping file that needs to be processed.
-        write_location: A string that points to a file directory.
-        filename: A string containing a filepath for where to write data to.
-
-    Returns:
-        None.
-    """
-
-    # get filename from url
-    file = re.sub('.gz|.zip', '', filename) if filename != '' else re.sub('.gz|.zip', '', url.split('/')[-1])
-
-    if '.zip' in url:
-        zipped_url_download(url, write_location, file)
-    elif '.gz' in url or '.gz' in filename:
-        if 'ftp' in url:
-            gzipped_ftp_url_download(url, write_location, file)
-        else:
-            gzipped_url_download(url, write_location, file)
-    else:
-        if 'ftp' in url:
-            ftp_url_download(url, write_location, file)
-        else:
-            url_download(url, write_location, file)
-
-    return None
 
 
 def data_frame_subsetter(data: pd.DataFrame, primary_key: str, subset_columns: List) -> pd.DataFrame:
@@ -274,7 +58,7 @@ def data_frame_subsetter(data: pd.DataFrame, primary_key: str, subset_columns: L
 
     for col in subset_columns:
         subset = data[[primary_key, col]]
-        subset['CODE_COLUMN'] = [col] * len(subset)
+        subset.loc[:, 'CODE_COLUMN'] = [col] * len(subset)
         subset.columns = [primary_key, 'CODE', 'CODE_COLUMN']
         subset_data_frames.append(subset)
 
--- tests/test_data_utils.py
+++ tests/test_data_utils.py
@@ -1,56 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import gzip
-import os.path
 import pandas as pd
-import requests
-import responses
-import shutil
 import unittest
-import urllib3
 
 from typing import Dict
 
 from omop2obo.utils import *
-
-# disable warnings
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
 class TestDataUtils(unittest.TestCase):
     """Class to test the downloading methods from the data utility script."""
 
     def setUp(self):
-        # create temporary directory to store data for testing
-        current_directory = os.path.dirname(__file__)
-        dir_loc = os.path.join(current_directory, 'data/temp')
-        self.dir_loc = os.path.abspath(dir_loc)
-        os.mkdir(self.dir_loc)
-
-        # create fake zipped data
-        empty_zip_data = b'1F   8B  08  00  00  00  00  00  00  0B'
-
-        with open(self.dir_loc + '/variant_summary.txt.gz', 'wb') as zp:
-            zp.write(empty_zip_data)
-
-        content = b'Lots of content here'
-        with gzip.open(self.dir_loc + '/variant_summary.txt.gz', 'wb') as f:
-            f.write(content)
-
-        # create some fake ftp data
-        with open(self.dir_loc + '/hgnc_complete_set.txt', 'w') as file:
-            file.write('None')
-
-        # set some urls
-        self.url = 'https://proconsortium.org/download/current/promapping.txt'
-        self.ftp_url = 'ftp://ftp.ebi.ac.uk/pub/databases/genenames/new/tsv/hgnc_complete_set.txt'
-        self.gzipped_ftp_url = 'ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/tab_delimited/variant_summary.txt.gz'
-        self.zipped_url = 'https://reactome.org/download/current/ReactomePathways.gmt.zip'
-        self.gzipped_url = 'https://www.disgenet.org/static/disgenet_ap1/files/downloads/disease_mappings.tsv.gz'
-
-        # set write location
-        self.write_location = self.dir_loc + '/'
 
         # create some fake Pandas DataFrames
         self.clin_data = pd.DataFrame({'CONCEPT_ID': ['4331309', '4331309', '37018594', '37018594', '442264'],
@@ -58,6 +20,7 @@ class TestDataUtils(unittest.TestCase):
                                        'UMLS_CUI': ['C0729608', 'C0729608', 'C4075981', 'C4075981', 'C0151936'],
                                        'UMLS_CODE': ['2265305', '2265305', '802510', '802510', '6817202']
                                        })
+
         self.subset_clin_data = pd.DataFrame({'CONCEPT_ID': ['4331309', '4331309', '37018594', '37018594', '442264',
                                                              '4331309', '4331309', '37018594', '37018594', '442264',
                                                              '4331309', '4331309', '37018594', '37018594', '442264'],
@@ -104,148 +67,6 @@ class TestDataUtils(unittest.TestCase):
                           'hyperekplexia 3': 'http://purl.obolibrary.org/obo/MONDO_0013827',
                           'color vision disorder': 'http://purl.obolibrary.org/obo/MONDO_0001703'}}
         }
-
-        return None
-
-    @responses.activate
-    def test_url_download(self):
-        """Tests url_download method"""
-
-        # filename
-        filename = self.url.split('/')[-1]
-
-        # fake file connection
-        responses.add(responses.GET,
-                      self.url,
-                      body='test',
-                      status=200,
-                      content_type='text/plain',
-                      headers={'Content-Length': '1200'}
-                      )
-
-        # test mocked download
-        url_download(self.url, self.write_location, filename)
-        self.assertTrue(os.path.exists(self.write_location + filename))
-
-        return None
-
-    @responses.activate
-    def test_ftp_url_download(self):
-        """Tests ftp_url_download method."""
-
-        filename = self.ftp_url.split('/')[-1]
-        self.assertTrue(os.path.exists(self.write_location + filename))
-
-        return None
-
-    @responses.activate
-    def test_gzipped_ftp_url_download(self):
-        """Tests gzipped_ftp_url_download method."""
-
-        # get ftp server info
-        file = self.gzipped_ftp_url.replace('ftp://', '').split('/')[-1]
-        write_loc = self.write_location + '{filename}'.format(filename=file)
-
-        # read in gzipped file,uncompress, and write to directory
-        with gzip.open(write_loc, 'rb') as fid_in:
-            with open(write_loc.replace('.gz', ''), 'wb') as f:
-                f.write(fid_in.read())
-        fid_in.close()
-
-        # change filename and remove gzipped and original files
-        os.remove(write_loc)
-
-        # test mocked download
-        self.assertFalse(os.path.exists(write_loc))
-        self.assertTrue(os.path.exists(write_loc[:-3]))
-
-        return None
-
-    @responses.activate
-    def test_zipped_url_download(self):
-        """Tests zipped_url_download methods."""
-
-        # filename
-        filename = self.zipped_url.split('/')[-1]
-
-        # fake file connection
-        responses.add(
-            responses.GET,
-            self.zipped_url,
-            body=b'1F   8B  08  00  00  00  00  00  00  0B',
-            status=200,
-            content_type='zip',
-            headers={'Content-Length': '1200'}
-        )
-
-        # test mocked download
-        # zipped_url_download(self.zipped_url, self.write_location, filename)
-        r = requests.get(self.zipped_url, allow_redirects=True)
-        self.assertTrue(r.ok)
-
-        # test writing data
-        downloaded_data = open(self.write_location + '{filename}'.format(filename=filename[:-4]), 'wb')
-        downloaded_data.write(r.content)
-        downloaded_data.close()
-
-        self.assertFalse(os.path.exists(self.write_location + filename))
-        self.assertTrue(os.path.exists(self.write_location + filename[:-4]))
-
-        return None
-
-    @responses.activate
-    def test_gzipped_url_download(self):
-        """Tests gzipped_url_download method when returning a 200 status."""
-
-        # filename
-        filename = self.gzipped_url.split('/')[-1]
-
-        # fake file connection
-        responses.add(
-            responses.GET,
-            self.gzipped_url,
-            body=gzip.compress(b'test data'),
-            status=200,
-            content_type='gzip',
-            headers={'Content-Length': '1200'}
-        )
-
-        # test mocked download
-        # gzipped_url_download(self.gzipped_url, self.write_location, filename)
-        r = requests.get(self.gzipped_url, allow_redirects=True, verify=False)
-        self.assertTrue(r.ok)
-
-        # test writing data
-        with open(self.write_location + '{filename}'.format(filename=filename[:-3]), 'wb') as outfile:
-            outfile.write(gzip.decompress(r.content))
-        outfile.close()
-        self.assertFalse(os.path.exists(self.write_location + filename))
-        self.assertTrue(os.path.exists(self.write_location + filename[:-3]))
-
-        return None
-
-    def test_data_downloader(self):
-        """Tests data_downloader method."""
-
-        # url data
-        data_downloader(self.url, self.write_location)
-        self.assertTrue(os.path.exists(self.write_location + self.url.split('/')[-1]))
-
-        # # ftp url data
-        self.assertTrue(os.path.exists(self.write_location + self.ftp_url.split('/')[-1]))
-
-        # gzipped ftp url data
-        file = self.gzipped_ftp_url.replace('ftp://', '').split('/')[-1]
-        write_loc = self.write_location + '{filename}'.format(filename=file)
-        self.assertTrue(os.path.exists(os.path.exists(write_loc[:-3])))
-
-        # zipped data
-        data_downloader(self.zipped_url, self.write_location)
-        self.assertTrue(os.path.exists(self.write_location + self.zipped_url.split('/')[-1][:-4]))
-
-        # gzipped data
-        data_downloader(self.gzipped_url, self.write_location)
-        self.assertTrue(os.path.exists(self.write_location + self.gzipped_url.split('/')[-1][:-3]))
 
         return None
 
@@ -310,11 +131,5 @@ class TestDataUtils(unittest.TestCase):
         self.assertIsInstance(combined_dicts, Dict)
         self.assertTrue(len(combined_dicts.keys()) == 6)
         self.assertTrue(len(combined_dicts.values()) == 6)
-
-        return None
-
-    def tearDown(self):
-        # remove temp directory
-        shutil.rmtree(self.dir_loc)
 
         return None
