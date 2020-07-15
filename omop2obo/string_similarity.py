4b2edd08880d267b68c7d19a46ee0eb71a9a6020
callahantiff <callahantiff@gmail.com>
2020-07-15 00:08:57 -0600

    nltk download workaround
    horrible workaround to avoid Travis error when trying to download nltk stopwords

--- omop2obo/string_similarity.py
+++ omop2obo/string_similarity.py
@@ -5,8 +5,6 @@
 # import needed libraries
 import hashlib
 import os
-import nltk
-nltk.download('wordnet')
 import numpy as np  # type: ignore
 import pandas as pd  # type: ignore
 import re
@@ -23,6 +21,24 @@ from tqdm import tqdm  # type: ignore
 from typing import Dict, List, Optional, Tuple
 
 from omop2obo.utils import column_splitter, data_frame_subsetter, merge_dictionaries
+
+# load stopwords into environment -- horrible workaround until Travis is fixed
+# nltk.download('wordnet')
+stopwords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd",
+             'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers',
+             'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what',
+             'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were',
+             'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the',
+             'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about',
+             'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from',
+             'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here',
+             'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other',
+             'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can',
+             'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain',
+             'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn',
+             "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn',
+             "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't",
+             'wouldn', "wouldn't"]
 
 
 class SimilarStringFinder(object):
@@ -102,7 +118,8 @@ class SimilarStringFinder(object):
         """
 
         col = [x for x in data.columns if x != primary_key][0]  # get text to perform preprocessing on
-        stop_word_patterns = re.compile(r'\b(' + r'|'.join(stopwords.words('english')) + r')\b\s*')
+        # stop_word_patterns = re.compile(r'\b(' + r'|'.join(stopwords.words('english')) + r')\b\s*')
+        stop_word_patterns = re.compile(r'\b(' + r'|'.join(stopwords) + r')\b\s*')
         lemmatizer = WordNetLemmatizer()
 
         # process data
