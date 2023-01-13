a2d1b5ed9fddd4c9abf36ec021e9193025899944
Tiffany J. Callahan <callahantiff@gmail.com>
2023-01-13 06:22:32 -0700

    adding missing functions

--- resources/analyses/analytic_utils.py
+++ resources/analyses/analytic_utils.py
@@ -4,16 +4,28 @@
 """
 Analytic Utility Functions.
 
-Clinical Data Manipulation
+Clinical Data Manipulation and Result Formatting
 * reconfigures_dataframe
-* splits_concept_levels
+* process_clinical_data
+* process_results
+* output_coverage_set_counts
+* process_error_analysis_data
 
-Ontology Data Manipulation
+Ontology Data Manipulation and Result Formatting
 * outputs_ontology_metadata
+
+Process Mapping Results
+* process_mapping_evidence
+* splits_concept_levels
 
 Statistical Testing
 * get_asterisks_for_pvalues
 * chisq_and_posthoc_corrected
+* min_max_scaler
+* gets_group_stats
+
+Plotting Functions
+* change_width
 
 """
 
@@ -23,8 +35,10 @@ import pandas as pd  # type: ignore
 
 from itertools import combinations  # type: ignore
 from scipy.stats import chi2_contingency  # type: ignore
+from sklearn import preprocessing
 from statsmodels.sandbox.stats.multicomp import multipletests  # type: ignore
 from typing import Dict, List, Optional
+
 
 
 def reconfigures_dataframe(split_list: List, data_frame: pd.DataFrame) -> pd.DataFrame:
@@ -102,15 +116,19 @@ def splits_concept_levels(data: pd.DataFrame, type_col: Optional[str], concept_s
     if type_col is not None:
         all_cols = [x for x in data.columns if type_col not in x]
         conc_type = [x for x in data.columns if con_string.upper() in x.upper() and type_col.upper() in x.upper()]
-        conc_type_uri = [x for x in conc_type if x.upper().endswith('URI')][0]
+        if len(conc_type) > 0:
+            conc_type_uri = [x for x in conc_type if x.upper().endswith('URI')][0]
+            concept = data[all_cols + conc_type].dropna(subset=conc_type, how='all').drop_duplicates()
+            concept_ont_codes = [i for j in [x.split(' | ') for x in list(concept[conc_type_uri])] for i in j]
+        else:
+            concept, concept_ont_codes = None, None
+
         anc_type = [x for x in data.columns if anc_string.upper() in x.upper() and type_col.upper() in x.upper()]
-        anc_type_uri = [x for x in anc_type if x.upper().endswith('URI')][0]
-        # extract concept codes from ancestor codes
-        concept = data[all_cols + conc_type].dropna(subset=conc_type, how='all').drop_duplicates()
-        ancestor = data[all_cols + anc_type].dropna(subset=anc_type, how='all').drop_duplicates()
-        # get counts of ontology concepts at each concept level
-        concept_ont_codes = [i for j in [x.split(' | ') for x in list(concept[conc_type_uri])] for i in j]
-        ancestor_ont_codes = [i for j in [x.split(' | ') for x in list(ancestor[anc_type_uri])] for i in j]
+        if len(anc_type) > 0:
+            anc_type_uri = [x for x in anc_type if x.upper().endswith('URI')][0]
+            ancestor = data[all_cols + anc_type].dropna(subset=anc_type, how='all').drop_duplicates()
+            ancestor_ont_codes = [i for j in [x.split(' | ') for x in list(ancestor[anc_type_uri])] for i in j]
+        else: ancestor, ancestor_ont_codes = None, None
     else:
         concept = data[[x for x in data.columns if x.startswith(con_string)]].dropna(how='all').drop_duplicates()
         ancestor = data[[x for x in data.columns if x.startswith(anc_string)]].dropna(how='all').drop_duplicates()
@@ -252,21 +270,153 @@ def process_clinical_data(data: pd.DataFrame, grp_var: str) -> Dict:
         grp_data = grouped_data.get_group(grp).drop_duplicates()
         results[grp]['grp_full_data'] = grp_data
         # get concept data
-        results[grp]['concept_src_code'] = [x for y in grp_data['CONCEPT_SOURCE_CODE'] for x in y.split(' | ')]
-        results[grp]['concept_src_label'] = [x for y in grp_data['CONCEPT_SOURCE_LABEL'] for x in y.split(' | ')]
-        results[grp]['concept_synonym'] = [x for y in grp_data['CONCEPT_SYNONYM'] for x in y.split(' | ')]
-        results[grp]['concept_vocab'] = ', '.join(set([x for y in grp_data['CONCEPT_VOCAB'] for x in y.split(' | ')]))
+        results[grp]['concept_src_code'] = [x for y in grp_data['CONCEPT_SOURCE_CODE'].astype(str) for x in y.split(' | ')]
+        if 'CONCEPT_SOURCE_LABEL' in grp_data.columns:
+            results[grp]['concept_src_label'] = [x for y in grp_data['CONCEPT_SOURCE_LABEL'].astype(str) for x in y.split(' | ')]
+        else: results[grp]['concept_src_label'] = [x for y in grp_data['CONCEPT_LABEL'].astype(str) for x in y.split(' | ')]
+        results[grp]['concept_synonym'] = [x for y in grp_data['CONCEPT_SYNONYM'].astype(str) for x in y.split(' | ')]
+        results[grp]['concept_vocab'] = ', '.join(set([x for y in grp_data['CONCEPT_VOCAB'].astype(str) for x in y.split(' | ')]))
         # get ancestor data
-        results[grp]['anc_concept_id'] = [x for y in grp_data['ANCESTOR_CONCEPT_ID'] for x in y.split(' | ')]
-        results[grp]['anc_src_code'] = [x for y in grp_data['ANCESTOR_SOURCE_CODE'] for x in y.split(' | ')]
-        results[grp]['anc_label'] = [x for y in grp_data['ANCESTOR_LABEL'] for x in y.split(' | ')]
-        results[grp]['anc_vocab'] = ', '.join(set([x for y in grp_data['ANCESTOR_VOCAB'] for x in y.split(' | ')]))
+        results[grp]['anc_concept_id'] = [x for y in grp_data['ANCESTOR_CONCEPT_ID'].astype(str) for x in y.split(' | ')]
+        results[grp]['anc_src_code'] = [x for y in grp_data['ANCESTOR_SOURCE_CODE'].astype(str) for x in y.split(' | ')]
+        results[grp]['anc_label'] = [x for y in grp_data['ANCESTOR_LABEL'].astype(str) for x in y.split(' | ')]
+        results[grp]['anc_vocab'] = ', '.join(set([x for y in grp_data['ANCESTOR_VOCAB'].astype(str) for x in y.split(' | ')]))
 
     return results
 
 
+def min_max_scaler(scores):
+    scaled_scores = []
+    for i in scores:
+        x = np.asarray(i).reshape(-1, 1)
+        scaler = preprocessing.MinMaxScaler()
+        x_scaled = scaler.fit_transform(x)
+        scaled_list = x_scaled.flatten().tolist()
+        scaled_scores += [scaled_list]
+
+    return scaled_scores
 
 
+def process_results(df, col_type, col_list):
+    if 'Concept Used In Practice' in set(df['CONCEPT_TYPE']):
+        df_grp = df.groupby('CONCEPT_TYPE')
+        df_grp_prac = df_grp.get_group('Concept Used In Practice').drop_duplicates()
+        df_grp_stnd = df_grp.get_group('Standard Concept Not Used In Practice').drop_duplicates()
+    else:
+        df_grp = df.groupby('CONCEPT_TYPE')
+        df_grp_prac = df_grp.get_group('CHCO Concept Used In Practice').drop_duplicates()
+        df_grp_stnd = df_grp.get_group('LOINC2HPO Concept').drop_duplicates()
+
+    # concepts used in practice
+    df_prac_concept_data, df_prac_ancestor_data = splits_concept_levels(df_grp_prac, col_type, col_list)
+
+    # standard concepts
+    df_stnd_concept_data, df_stnd_ancestor_data = splits_concept_levels(df_grp_stnd, col_type, col_list)
+
+    results = [df_prac_concept_data, df_prac_ancestor_data, df_stnd_concept_data, df_stnd_ancestor_data]
+
+    return df_grp_prac, df_grp_stnd, results
 
 
+def process_mapping_evidence(evidence_list):
+    evidence_dict = {'all': [], 'dbxref': [], 'synonym': [], 'label': [], 'similarity': []}
 
+    for x in evidence_list:
+        for i in x.split(' | '):
+            if 'synonym' in i.lower():
+                evidence_dict['synonym'] += [i.split(':')[-1]]
+                evidence_dict['all'] += [i.split(':')[-1]]
+            if 'dbxref' in i.lower():
+                evidence_dict['dbxref'] += [i.split(':')[-1]]
+                evidence_dict['all'] += [i.split(':')[-1]]
+            if 'label' in i.lower():
+                evidence_dict['label'] += [i.split(':')[-1]]
+                evidence_dict['all'] += [i.split(':')[-1]]
+            if 'similarity' in i.lower():
+                evidence_dict['similarity'] += [float(i.split(':')[-1].split('_')[-1])]
+                evidence_dict['all'] += [i.split(':')[0] + ':' + '_'.join(i.split(':')[-1].split('_')[:-1])]
+
+    return evidence_dict
+
+
+def output_coverage_set_counts(cp_data, chco_data, sets):
+    set_counts = {'overlap': None, 'cp_only': None, 'omop2obo_only': None}
+    overlap, cp_only, o2o_only = sets
+
+    # overlap
+    #     set_counts['overlap'] = chco_data[chco_data['CONCEPT_ID'].isin(overlap)][[
+    #     'CONCEPT_COUNT_ADJUSTED']].dropna().values.flatten().tolist()
+    set_counts['overlap'] = cp_data[cp_data['CONCEPT_ID'].isin(overlap)][['RECORD_COUNT']].values.flatten().tolist()
+
+    # cp only
+    set_counts['cp_only'] = cp_data[cp_data['CONCEPT_ID'].isin(cp_only)][['RECORD_COUNT']].values.flatten().tolist()
+
+    # o2o only
+    set_counts['omop2obo_only'] = chco_data[chco_data['CONCEPT_ID'].isin(o2o_only)][
+        ['CONCEPT_COUNT_ADJUSTED']].dropna().values.flatten().tolist()
+
+    return set_counts
+
+
+def process_error_analysis_data(error_analysis, purposefully_unmapped, missing_concepts, cp_data, chco_data):
+    set_counts = {'cdm_version': None, 'purposefully_excluded': None, 'missing': None}
+
+    # covered by error analysis
+    new_cdm_df = cp_data.merge(error_analysis, left_on='CONCEPT_ID', right_on='TARGET_CONCEPT_ID', how='inner')
+    new_cdm = new_cdm_df[new_cdm_df['CONCEPT_ID'].isin(missing_concepts)]
+    new_cdm_concepts = set(new_cdm['CONCEPT_ID'])
+    set_counts['cdm_version'] = {
+        'concepts': new_cdm,
+        'counts': new_cdm[['RECORD_COUNT']].values.flatten().tolist()
+    }
+    new_cdm_concepts = set(new_cdm['CONCEPT_ID'])
+
+    # covered by excluded mapping concepts (anything not mapped that's not None)
+    updated_missing_concepts = missing_concepts - new_cdm_concepts
+    excluded_df = cp_data[cp_data['CONCEPT_ID'].isin(list(purposefully_unmapped['CONCEPT_ID']))]
+    excluded = excluded_df[excluded_df['CONCEPT_ID'].isin(updated_missing_concepts)]
+    excluded = excluded.merge(chco_data, on='CONCEPT_ID', how='inner')
+
+    excluded_concepts = set(excluded['CONCEPT_ID'])
+    set_counts['purposefully_excluded'] = {
+        'concepts': excluded,
+        'counts': excluded[['RECORD_COUNT']].values.flatten().tolist()
+    }
+
+    # remaining concepts not accounted for concepts
+    final_missing_concepts = missing_concepts - (new_cdm_concepts | excluded_concepts)
+    set_counts['missing'] = {
+        'concepts': cp_data[cp_data['CONCEPT_ID'].isin(final_missing_concepts)],
+        'counts': cp_data[cp_data['CONCEPT_ID'].isin(final_missing_concepts)][
+            ['RECORD_COUNT']].values.flatten().tolist()
+    }
+
+    return set_counts
+
+
+def gets_group_stats(cp_data, chco_data, grouper, primary_key):
+    coverage_dict = dict()
+    dbs_db = cp_data.groupby(grouper)
+    db_list = set(cp_data[grouper])
+
+    for x in db_list:
+        x_db_df = dbs_db.get_group(x)
+        covered = x_db_df[x_db_df[primary_key].isin(chco_data[primary_key])]
+        missing = x_db_df[~x_db_df[primary_key].isin(chco_data[primary_key])]
+        coverage_dict[x] = {'overlap': dict(zip(list(covered[primary_key]), list(covered['RECORD_COUNT']))),
+                            'primary_only': dict(zip(list(missing[primary_key]), list(missing['RECORD_COUNT'])))
+                            }
+
+    return coverage_dict
+
+
+def change_width(ax, new_value) :
+    for patch in ax.patches :
+        current_width = patch.get_width()
+        diff = current_width - new_value
+
+        # we change the bar width
+        patch.set_width(new_value)
+
+        # we recenter the bar
+        patch.set_x(patch.get_x() + diff * .5)
