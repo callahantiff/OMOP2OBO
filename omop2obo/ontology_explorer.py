300f483a904d0a82116819797dfbaeab6e04bd73
callahantiff <callahantiff@gmail.com>
2023-03-27 15:38:41 -0400

    adding code to pickle master dictionary

--- omop2obo/ontology_explorer.py
+++ omop2obo/ontology_explorer.py
@@ -7,8 +7,6 @@ import glob
 import json
 import os
 import pandas  # type: ignore
-import pickle
-import sys
 
 from rdflib import Graph, Literal, Namespace, URIRef  # type: ignore
 from rdflib.namespace import RDF, RDFS, OWL  # type: ignore
@@ -27,12 +25,12 @@ class OntologyInfoExtractor(object):
     """Class processes downloaded OBO ontology OWL files. The data are processed in order to obtain important metadata
     like labels, definitions, synonyms, and database cross-references and writes the data as a Pandas DataFrame
     object. The class produces the following three files for each processed ontology (examples of the data stored in
-    each object are provided in the corresponding methods:
+    each object are provided in the corresponding methods):
         1. resources/ontologies/hp_ontology_hierarchy_information.pkl
         2. resources/ontologies/hp_ontology_ancestors.json
         3. resources/ontologies/hp_ontology_children.json
 
-    As each ontology is processed, the master_ontology_dictionary object is built, which is the primary output that
+    As each ontology is processed, the master_ontology_dictionary object is built, which stores the primary output that
     is utilized by the OMOP2OBO algorithm. An example of the data stored in this object is shown below.
         {'hp': {
                 'df': 'resources/ontologies/hp_ontology_hierarchy_information.pkl',
@@ -64,7 +62,7 @@ class OntologyInfoExtractor(object):
         else: self.ont_directory = ontology_directory
 
     def get_ontology_information(self, ont_id: str) -> Dict:
-        """Queries an rdflib Graph object and returns labels, definitions, dbXRefs, and synonyms for all
+        """Queries a rdflib Graph object and returns labels, definitions, dbXRefs, and synonyms for all
         non-deprecated and obsolete ontology classes.
 
         Args:
@@ -79,21 +77,21 @@ class OntologyInfoExtractor(object):
         """
 
         # identify current ontology classes in ont_id namespace
-        print('.Querying Ontology to Obtain all OWL:Class Objects')
+        print('\t- Querying Ontology to Obtain all OWL:Class Objects')
         ont_classes = gets_ontology_classes(self.graph, ont_id)
-        print('. .Querying Ontology to Obtain all Deprecated OWL:Class Objects')
+        print('\t- Querying Ontology to Obtain all Deprecated OWL:Class Objects')
         deprecated = gets_deprecated_ontology_classes(self.graph, ont_id)
-        print('. . .Querying Ontology to Obtain all Obsolete OWL:Class Objects')
+        print('\t- Querying Ontology to Obtain all Obsolete OWL:Class Objects')
         obsolete = gets_obsolete_ontology_classes(self.graph, ont_id)
         filtered_classes = set([x for x in ont_classes if x not in deprecated | obsolete])
         # obtain class-level metadata
-        print('. . . .Querying Ontology to Obtain all OWL:Class Object Labels')
+        print('\t- Querying Ontology to Obtain all OWL:Class Object Labels')
         ont_labels = gets_ontology_class_labels(self.graph, filtered_classes)
-        print('. . . . .Querying Ontology to Obtain all OWL:Class Object Definitions')
+        print('\t- Querying Ontology to Obtain all OWL:Class Object Definitions')
         ont_definitions = gets_ontology_class_definitions(self.graph, filtered_classes)
-        print('. . . . . .Querying Ontology to Obtain all OWL:Class Object DbXRefs')
+        print('\t- Querying Ontology to Obtain all OWL:Class Object DbXRefs')
         ont_dbx = gets_ontology_class_dbxrefs(self.graph, filtered_classes)
-        print('. . . . . . .Querying Ontology to Obtain all OWL:Class Object Synonyms')
+        print('\t- Querying Ontology to Obtain all OWL:Class Object Synonyms')
         ont_synonyms = gets_ontology_class_synonyms(self.graph, filtered_classes)
         # store results as dictionary
         res: Dict = {'label': ont_labels, 'definition': ont_definitions, 'dbxref': ont_dbx, 'synonym': ont_synonyms}
@@ -133,28 +131,31 @@ class OntologyInfoExtractor(object):
         ns = str(ns[0][2]) if len(ns) > 0 else ont_id
         sab = str(list(self.graph.triples((None, OWL.versionIRI, None)))[0][2])
         # process labels, definitions, and synonyms
-        labs = pandas.DataFrame({'obo_id': k, 'code': k.split('/')[-1].replace('_', ':'), 'string': v,
-                                 'string_type': 'class label'} for k, v in res['label'].items())
-        defs = pandas.DataFrame({'obo_id': k, 'code': k.split('/')[-1].replace('_', ':'), 'string': v,
-                                 'string_type': 'class definition'} for k, v in res['definition'].items())
-        syn = pandas.DataFrame({'obo_id': y[0], 'code': y[0].split('/')[-1].replace('_', ':'), 'string': x[0],
-                                'string_type': x[1]} for y in [(k, v) for k, v in res['synonym'].items()] for x in y[1])
+        labs = pandas.DataFrame(
+            {'obo_id': k, 'code': k.split('/')[-1].replace('_', ':'), 'string': v, 'string_type': 'class label'}
+            for k, v in res['label'].items())
+        defs = pandas.DataFrame(
+            {'obo_id': k, 'code': k.split('/')[-1].replace('_', ':'), 'string': v, 'string_type': 'class definition'}
+            for k, v in res['definition'].items())
+        syn = pandas.DataFrame(
+            {'obo_id': y[0], 'code': y[0].split('/')[-1].replace('_', ':'), 'string': x[0], 'string_type': x[1]}
+            for y in [(k, v) for k, v in res['synonym'].items()] for x in y[1])
         ont_df = pandas.concat([labs, defs, syn]).drop_duplicates()
         # process dbxrefs
-        dbx = pandas.DataFrame({'obo_id': y[0], 'code': y[0].split('/')[-1].replace('_', ':'), 'dbx': x[0],
-                                'dbx_type': x[1], 'dbx_source': x[2], 'dbx_source_name': x[2]}
-                               for y in [(k, v) for k, v in res['dbxref'].items()] for x in y[1])
+        dbx = pandas.DataFrame(
+            {'obo_id': y[0], 'code': y[0].split('/')[-1].replace('_', ':'), 'dbx': x[0], 'dbx_type': x[1],
+             'dbx_source': x[2], 'dbx_source_name': x[2]}
+            for y in [(k, v) for k, v in res['dbxref'].items()] for x in y[1])
         ont_df = ont_df.merge(dbx, on=['obo_id', 'code'], how='left').drop_duplicates()
         # add metadata
         ont_df['obo_source'] = sab; ont_df['obo_semantic_type'] = ns
         ont_df = ont_df.fillna('None').drop_duplicates()
         # write data to local directory (resources/ontologies)
+        print('\t- Writing Concept Ancestor and Descendant Dictionaries to Disk')
         out = '{}/{}_ontology_hierarchy_information.pkl'.format(self.ont_directory, ont_id)
-        max_bytes, bytes_out = 2 ** 31 - 1, pickle.dumps(ont_df); n_bytes = sys.getsizeof(bytes_out)
-        with open(out, 'wb') as f_out:
-            for idx in range(0, n_bytes, max_bytes): f_out.write(bytes_out[idx:idx + max_bytes])
+        pickle_large_data_structure(ont_df, out)
+        print('\t  ...Wrote Pandas DataFrame to: {}'.format(out))
         self.master_ontology_dictionary[ont_id]['df'] = out
-        print('.Wrote Pandas DataFrame to: {}'.format(out))
 
         return ont_df
 
@@ -177,20 +178,21 @@ class OntologyInfoExtractor(object):
             None.
         """
 
-        print('. Obtaining Ontology Concept Ancestors')
+        print('\t- Obtaining Ontology Concept Ancestors')
         cls = set(ont_df['obo_id'])
         obo_ancs = {x: entity_search(self.graph, x, 'ancestors', ont_id.upper(), RDFS.subClassOf) for x in tqdm(cls)}
-        print('. . Obtaining Ontology Descendants. Please be patient, this can take several minutes.')
+        print('\t- Obtaining Ontology Descendants. Please be patient, this can take several minutes.')
         obo_kids = {x: entity_search(self.graph, x, 'children', ont_id.upper(), RDFS.subClassOf) for x in tqdm(cls)}
 
         # write results to local directory (resources/ontologies)
+        print('\t- Writing Concept Ancestor and Descendant Dictionaries to Disk')
         anc_file_str, kid_file_str = '{}/{}_ontology_ancestors.json', '{}/{}_ontology_children.json'
         self.master_ontology_dictionary[ont_id]['ancestors'] = anc_file_str.format(self.ont_directory, ont_id)
         self.master_ontology_dictionary[ont_id]['children'] = kid_file_str.format(self.ont_directory, ont_id)
+        print('\t  ...Wrote Ancestor Dictionary to: {}'.format(self.master_ontology_dictionary[ont_id]['ancestors']))
         json.dump(obo_ancs, open(self.master_ontology_dictionary[ont_id]['ancestors'], 'w'))
+        print('\t  ...Wrote Descendant Dictionary to: {}'.format(self.master_ontology_dictionary[ont_id]['children']))
         json.dump(obo_kids, open(self.master_ontology_dictionary[ont_id]['children'], 'w'))
-        print('. . .Wrote Ancestor Dictionary to: {}'.format(self.master_ontology_dictionary[ont_id]['ancestors']))
-        print('. . . . Wrote Descendant Dictionary to: {}'.format(self.master_ontology_dictionary[ont_id]['children']))
 
         return None
 
@@ -204,16 +206,23 @@ class OntologyInfoExtractor(object):
             None.
         """
 
+        print('\n' + '===' * 15 + '\nPROCESSING OBO FOUNDRY ONTOLOGIES DATA\n' + '===' * 15)
+
         for ont in self.ont_dictionary.items():
             self.master_ontology_dictionary[ont[0]] = {'df': None, 'ancestors': None, 'children': None}
-            print('===' * 15 + '\nPROCESSING ONTOLOGY: {}\n'.format(ont[0]) + '===' * 15)
-            print('STEP 1: Loading Data...Please be patient, this step can take several minutes.')
+            print('*' * 10 + ' PROCESSING ONTOLOGY: {} '.format(ont[0]) + '*' * 10)
+            print('--> Loading Data...Please be patient, this step can take several minutes.')
             self.graph = Graph().parse(ont[1], format='xml')
-            print('\nSTEP 2: Obtaining Ontology Metadata')
+            print('--> Obtaining Ontology Metadata')
             ont_dict = self.get_ontology_information(ont[0])
-            print('\nSTEP 3: Converting Metadata into Pandas DataFrame')
+            print('--> Converting Metadata into Pandas DataFrame')
             ont_df = self.creates_pandas_dataframe(ont_dict, ont[0])
-            print('\nSTEP 4: Obtaining Ancestors and Descendants for each Ontology Class')
+            print('--> Obtaining Ancestors and Descendants for each Ontology Class')
             self.ontology_entity_finder(ont_df, ont[0])
+
+        # write dictionary containing all ontologies
+        f1 = 'resources/ontologies/processed_obo_data_dictionary.pkl'
+        print('\t- Writing Processed OBO Foundry Dictionary: "{}"'.format(f1))
+        pickle_large_data_structure(self.master_ontology_dictionary, f1)
 
         return None
